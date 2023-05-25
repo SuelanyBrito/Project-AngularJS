a973b906f29267995179017170b304480514e582
Suelany Brito <Suelanybr@gmail.com>
2023-05-25 12:07:00 -0300

    Add teacher form

--- src/scripts/controllers/teacher.js
+++ src/scripts/controllers/teacher.js
@@ -1,3 +1,22 @@
 app.controller('TeacherController', function ($scope) { 
-    $scope.title = 'HELLO WORLD!!'
+    $scope.teachers = []; 
+    
+    $scope.registrationTeacher = function () {
+        // Lógica para cadastrar um novo aluno
+        var newTeacher = {
+          name: $scope.fullName = '',
+          email: $scope.email = '',
+          password: $scope.password = '',
+          occupation: $scope.occupation = '',
+          formation: $scope.formation = ''
+        };
+        $scope.teachers.push(newTeacher);
+  
+        // Limpar os campos do formulário
+        $scope.name = '';
+        $scope.email = '';
+        $scope.password = '';
+        $scope.occupation = '';
+        $scope.formation = '';
+      };
 });
---
+++ src/scripts/directives/registration_teacher.js
@@ -0,0 +1,7 @@
+app.directive('registrationTeacher', function () {
+    return {
+      restrict: 'E',
+      templateUrl: 'scripts/templates/registration-teacher.html',
+      controller: 'TeacherController'
+    };
+  });
