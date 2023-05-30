c09b7a2023a32ab0f716367c2a7e0bf81f9585db
Suelany Brito <Suelanybr@gmail.com>
2023-05-30 19:05:29 -0300

    add server.js

---
+++ server.js
@@ -0,0 +1,56 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const port = 8000;
+
+const server = http.createServer((req, res) => {
+  let filePath = '.' + req.url;
+  if (filePath === './') {
+    filePath = './app/index.html';
+  }else if(filePath === './home') {
+    filePath = './app/features/home/home.html';
+  }else if(filePath === './register') {
+    filePath = './app/features/register/register.html';
+  }
+
+  const extname = path.extname(filePath);
+  let contentType = 'text/html';
+
+  switch (extname) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.jpg':
+      contentType = 'image/jpg';
+      break;
+  }
+
+  fs.readFile(filePath, (error, content) => {
+    if (error) {
+      if (error.code === 'ENOENT') {
+        res.writeHead(404);
+        res.end('File not found');
+      } else {
+        res.writeHead(500);
+        res.end('Internal server error');
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
