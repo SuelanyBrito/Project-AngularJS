d7ee07c2493906d3fbecaee5db1178fea1291693
Suelany Brito <Suelanybr@gmail.com>
2023-06-19 18:27:15 -0300

    edit project details page

--- app/features/project/projectDetails/projectDetails.js
+++ app/features/project/projectDetails/projectDetails.js
@@ -1,21 +1,8 @@
-app.controller('ProjectDetailsController', ['$scope', '$location', function ($scope, $location) { 
-    $scope.nameProject = "Name of Project";
-    $scope.description = "Lorem Ipsum dolor sit amet, consectetur adipis, sed do eiusmod tempor incididunt ut labore et dol, itaque, sed do eiusmod tempor incididunt, ullamcorper itaque";
-    
-    $scope.projectCoordinator = "Adalberto";
-    $scope.students = [
-        {
-            name: "Suelany",
-            class: "Júnior"
-        },
-        {
-            name: "Leticia",
-            class: "Pleno"
-        }
-    ]
-    $scope.upload = new Date(2022,17,7)
+app.controller('ProjectDetailsController', function ($scope, ProjectService) { 
+    $scope.project = ProjectService.getProjectSelect();
+    console.log($scope.project);
     $scope.comments = ""
     $scope.typeDocument = "Document - Any Generic Format"
     $scope.format = "pdf"
     $scope.fileSize = '820Kb'
-}]);
+});
--- app/features/project/projectService/projectService.js
+++ app/features/project/projectService/projectService.js
@@ -1,12 +1,29 @@
 app.service('ProjectService', function() {
     var dataList = [];
+
+    var projectselect = '';
   
-    this.addData = function(newData) {
-      dataList.push(newData);
+    this.addData = function(project) {
+      dataList.push({
+        name: project.name,
+        description: project.description,
+        date: new Date(2023, 6, 19),
+        coordinator: project.coordinator,
+        students: project.students,
+      });
     };
   
     this.getDataList = function() {
       return dataList;
     };
-  });
+
+    this.setProjectSelect = function(project) {
+      projectselect = project;
+    };
+
+    this.getProjectSelect = function() {
+      return projectselect;
+    };
+
+});
   
