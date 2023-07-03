ade3a21a1048c052a7fc5c7e4a3c2e33e536065e
leticiagc <leticia.calixto@ccc.ufcg.edu.br>
2023-07-03 13:03:26 -0300

    project list structure refactoring

--- app/features/project/projectList/projectList.js
+++ app/features/project/projectList/projectList.js
@@ -20,7 +20,24 @@ app.controller('ProjectListController', function (ProjectService, $scope, $locat
 
     $scope.selectedPage = $scope.pages[0];  
 
+    // $scope.paginate = $scope.calculateView;
+    // $scope.currentPage = 1; // a definir
+
+    // $scope.calculateView = function () {
+    //   var startIndex = ($scope.currentPage - 1) * $scope.selectedPage;
+    //   var endIndex = startIndex + $scope.selectedPage;
+    //   return $scope.dataList.slice(startIndex, endIndex);
+    // };
+
     $scope.numberOfPages = function(){
         return Math.ceil($scope.sizeList/$scope.selectedPage);
     }
+
+    // $scope.updatePages = function(){
+    //     $scope.paginate
+    // }
+
+    // $scope.getRange = function (num) {
+    //     return Array.from({ length: num }, (_, index) => index + 1);
+    //   };
 });
